2dca4b02e5f9a03f1f1e1530b01454e2190b98ac
Matt Berlin <matt@cranklogic.com>
2018-04-20 10:57:02 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World 2!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
